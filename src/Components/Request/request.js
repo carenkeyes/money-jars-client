7df1b17081192cbffcea913d8ba6736149d1d9fe
Caren Keyes <cejkeyes@gmail.com>
2018-07-23 11:54:13 -0700

    add componenet for authorizing ynab

---
+++ src/Components/Request/request.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import API_BASE_URL from '../../../config';
+
+export default class Request extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <a href="">Authorize YNAB</a>    
+            </div>
+        )
+    }
+}
