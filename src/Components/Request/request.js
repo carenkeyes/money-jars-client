2f805b7ffeb65be24f794293944b208daed1e308
Caren Keyes <cejkeyes@gmail.com>
2018-07-23 12:09:40 -0700

    add path to request authorization url from server

--- src/Components/Request/request.js
+++ src/Components/Request/request.js
@@ -2,11 +2,50 @@ import React from 'react';
 import API_BASE_URL from '../../../config';
 
 export default class Request extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            url: "",
+            error: null,
+            loading: false
+        }
+    }
+
+    componenetDidMount(){
+        this.getUrl
+    }
+
+    getUrl(){
+        this.setState({
+            error: null,
+            loading: true
+        });
+        return fetch(`${API_BASE_URL}/auth`)
+            .then(res => {
+                if(!res.ok){
+                    return Promise.reject(res.statusText);
+                }
+                return res.json();
+            })
+            .then(y =>
+                this.setState({
+                    url: y.url,
+                    loading: false
+                })
+            )
+            .catch(err => 
+                this.setState({
+                    error: 'Could not load url',
+                    loading: false
+                }))
+    }
 
     render(){
+        let authURL = this.state.url;
         return(
             <div>
-                <a href="">Authorize YNAB</a>    
+                <a href={authURL}>Authorize YNAB</a>    
             </div>
         )
     }
