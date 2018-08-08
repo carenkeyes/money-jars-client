f29658feac5574635e0a9a2b3686b6c965d714f6
Caren Keyes <cejkeyes@gmail.com>
2018-08-08 11:25:55 -0700

    add redirect to home page

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
 
-export default function Home(){
+
+
+
+export function Home(props){
+    if(props.loggedIn){
+        return <Redirect to='/parent' />;
+    }
     return (
         <p>Home</p>
-    )
+    );
 }
+
+const mapStatetoProps = state => ({
+    loggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStatetoProps)(Home)
