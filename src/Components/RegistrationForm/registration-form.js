e523b1bdacd8fc603d4b89286aa51aa9c3c51f18
Caren Keyes <cejkeyes@gmail.com>
2018-08-08 11:19:21 -0700

    basic registration form

--- src/Components/RegistrationForm/registration-form.js
+++ src/Components/RegistrationForm/registration-form.js
@@ -2,4 +2,59 @@ import React from 'react';
 import {Field, reduxForm, focus} from 'redux-form';
 import {registerUser} from '../../actions';
 import {login} from '../../actions/auth';
+import Input from './input';
+import {required, nonEmpty, matches, length, isTrimmed} from '../../validators';
+import { normalizeResponseErrors } from '../../actions/utils';
 
+const passwordLength = length({min: 10, max: 72});
+const matchesPassword = matches('password');
+
+export class RegistrationForm extends React.Component {
+    onSubmit(values){
+        const {usernmae, password, firstName, lastName} = values;
+        const user = {username, password, firstName, lastName};
+        return this.props
+            .dispatch(registerUser(user))
+            .then(() => this.props.dispatch(login(username, password)));
+    }
+
+    render(){
+        return (
+            <form  
+                className="login-form"
+                onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
+                <label htmlfor="username">Username</label>
+                <Field
+                    component={Input}
+                    type="text"
+                    name="username"
+                    validate={[required, nonEmpty, isTrimmed]}
+                />
+                <label htmlFor="password">Password</label>
+                <Field
+                    component={Input}
+                    type="password"
+                    name="password"
+                    validate={[required, passwordLength, isTrimmed]}
+                />
+                <label htmlFor="passwordConfirm">Confirm password</label>
+                <Field  component={Input}
+                type="password"
+                name="passwordConfirm"
+                validate={[required, nonEmpty, matchesPassword]}
+                />
+                <button
+                    type="submit"
+                    disables={this.props.pristing || this.props.submitting}>
+                    Register
+                </button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'registration',
+    onSubmitFail: (errors, dispatch) =>
+        dispatch(focus('registration', Object.keys(errors)[0]))
+})(RegistrationForm);
