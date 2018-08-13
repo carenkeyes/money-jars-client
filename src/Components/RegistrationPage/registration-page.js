853ffdd6d855e75f00174415e2f22436f46af9ef
Caren Keyes <cejkeyes@gmail.com>
2018-08-13 11:07:53 -0700

    add basic styling to registration page

--- src/Components/RegistrationPage/registration-page.js
+++ src/Components/RegistrationPage/registration-page.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import './registration-page.css';
 
 import RegistrationForm from '../RegistrationForm/registration-form';
 import LoginForm from '../LoginForm/login-form';
 
 export function RegistrationPage(props){
     return(
-        <div>
+        <div className='registration'>
             <Route exact path={`${props.match.url}/login`} component={LoginForm} />
             <Route exact path={`${props.match.url}/signup`} component={RegistrationForm} />
         </div>
