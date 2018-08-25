c31527a751bbf3098a87b152d831893628af5746
Caren Keyes <cejkeyes@gmail.com>
2018-08-24 18:13:45 -0700

    CSS and images for goals

--- src/Components/Avatar/avatar.js
+++ src/Components/Avatar/avatar.js
@@ -6,9 +6,15 @@ export default class Avatar extends React.Component{
     render(){
         if(!this.props.className){
             return null;
+        } else if(this.props.className === 'avatarImage'){
+            return(
+                <div className={`${this.props.className}`}>
+                    <img src={this.props.image} alt='' />
+                </div>
+            )
         }
         return(
-            <div className={`avatarImage ${this.props.className}`}>
+            <div className={`${this.props.className}`}>
             </div>
         )
     }
--- src/reducers/budget.reducer.js
+++ src/reducers/budget.reducer.js
@@ -8,7 +8,7 @@ const initialState = {
             title: "Bicycle",
             amount: 80,
             saved: 10,
-            category: "saving",
+            category: "Saving",
             imageurl: 'https://i5.walmartimages.com/asr/c4061a3b-3151-4a9c-b168-5a5449e35d10_1.cb67f779006e94571d75d44a0ddd62aa.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF'
 
         },
@@ -17,16 +17,14 @@ const initialState = {
             title: "Christmas",
             amount: 50,
             saved: 2,
-            category: "giving",
-            imageurl: 'https://photos.smugmug.com/Web-images/n-dfnXHf/i-8BPTkJv/0/22ac2d0b/S/i-8BPTkJv-S.png'
+            category: "Saving",
         },
         {
             id: '3',
             title: "Dad's birthday",
             amount: 10,
             saved: 9,
-            category: "giving",
-            imageurl: 'https://photos.smugmug.com/Web-images/n-dfnXHf/i-8BPTkJv/0/22ac2d0b/S/i-8BPTkJv-S.png'
+            category: "Giving",
         }
     ]
 }
