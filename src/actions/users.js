422d341cdc9b680e0dbd7942e9412dc28ed5fbb7
Caren Keyes <cejkeyes@gmail.com>
2018-08-29 11:12:07 -0700

    update actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,26 +1,92 @@
 import {SubmissionError} from 'redux-form';
-
 import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './utils';
+import {push} from 'connected-react-router';
 
-export const registerUser = user => dispatch => {
-    return fetch(`${API_BASE_URL}/user`, {
-        method: 'POST',
+export const FETCH_USER_BASIC_INFO_REQUEST_TRIGGERED = 'FETCH_USER_BASIC_INFO_REQUEST_TRIGGERED';
+export const FETCH_USER_BASIC_INFO_REQUEST_SUCCESS = 'FETCH_USER_BASIC_INFO_REQUEST_SUCCESS';
+export const FETCH_USER_BASIC_INFO_REQUEST_FAILURE = 'FETCH_USER_BASIC_INFO_REQUEST_FAILURE';
+
+export function fetchUserBasicInfo() {
+    const promise = fetch(`${appConfig.USER_ENDPOINT}`, {
         headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    })
-    .then(res => normalizeResponseErrors(res))
-    .then(res => res.json())
-    .catch(err => {
-        const {reason, message, location} = err;
-        if (reason === 'ValidationError'){
-            return Promise.reject(
-                new SubmissionError({
-                    [location]: message 
-                })
-            );
+            'Content-Type': 'application/json',
+            Authorization: sessionStorage.getItem(appConfig.TOKEN_CONTENT_KEY)
         }
     });
+    return {
+        onRequest: FETCH_USER_BASIC_INFO_REQUEST_TRIGGERED,
+        onSuccess: FETCH_USER_BASIC_INFO_REQUEST_SUCCESS,
+        onFailure: FETCH_USER_BASIC_INFO_REQUEST_FAILURE,
+        promise,
+    };
+}
+
+export const FETCH_USER_LOGIN_REQUEST_TRIGGERED = 'FETCH_USER_LOGIN_REQUEST_TRIGGERED';
+export const FETCH_USER_LOGIN_REQUEST_SUCCESS = 'FETCH_USER_LOGIN_REQUEST_SUCCESS';
+export const FETCH_USER_LOGIN_REQUEST_FAILURE = 'FETCH_USER_LOGIN_REQUEST_FAILURE';
+
+const handleLoginResponse = (response, dispatch) => {
+  sessionStorage.setItem(appConfig.TOKEN_CONTENT_KEY, response.token);
+  dispatch({
+      type: FETCH_USER_LOGIN_REQUEST_SUCCESS,
+      response,
+  });
+  dispatch(push('/dashboard'));
 };
+
+
+export const FETCH_USER_SIGNUP_REQUEST_TRIGGERED = 'FETCH_USER_SIGNUP_REQUEST_TRIGGERED';
+export const FETCH_USER_SIGNUP_REQUEST_SUCCESS = 'FETCH_USER_SIGNUP_REQUEST_SUCCESS';
+export const FETCH_USER_SIGNUP_REQUEST_FAILURE = 'FETCH_USER_SIGNUP_REQUEST_FAILURE';
+export const CREATE_USER_REQUEST_SUCCESS = 'CREATE_USER_REQUEST_SUCCESS';
+
+export function createUser(name, email, password, phoneNumber, address, is_driver) {
+    const promise = fetch(`${appConfig.USER_CREATE}`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          email,
+          name,
+          password,
+          phoneNumber,
+          address,
+          password,
+          is_driver,
+        }),
+    });
+    return {
+        onRequest: FETCH_USER_SIGNUP_REQUEST_TRIGGERED,
+        onSuccess: handleCreateUserResponse,
+        onFailure: FETCH_USER_SIGNUP_REQUEST_FAILURE,
+        promise,
+    };
+  }
+
+
+const handleCreateUserResponse = (response, dispatch) => {
+    sessionStorage.setItem(appConfig.TOKEN_CONTENT_KEY, response.token);
+    dispatch({
+        type: CREATE_USER_REQUEST_SUCCESS,
+        response,
+    });
+    dispatch(push('/dashboard'));
+};
+  
+
+export function fetchUserLogin(email, password) {
+  const promise = fetch(`${appConfig.USER_ENDPOINT}`, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+  });
+  return {
+      onRequest: FETCH_USER_LOGIN_REQUEST_TRIGGERED,
+      onSuccess: handleLoginResponse,
+      onFailure: FETCH_USER_LOGIN_REQUEST_FAILURE,
+      promise,
+  };
+}
