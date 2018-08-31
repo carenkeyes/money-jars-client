6d534026905ea298df8003caec4a5e8f0d581752
Caren Keyes <cejkeyes@gmail.com>
2018-08-31 16:05:12 -0700

    initial smoke test for App component

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({adapter: new Adapter()});
+
+describe('<App />', () =>{
+  it('Should render without crashing', () => {
+    shallow(<App />)
+  })
+})
