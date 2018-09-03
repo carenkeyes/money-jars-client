e961155fdda6f554f4cedcc740cabb1caa00926c
Caren Keyes <cejkeyes@gmail.com>
2018-09-02 18:40:37 -0700

    buttons change if logged on

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -4,9 +4,9 @@ import {Link} from 'react-router-dom';
 import Button from '../Button/button';
 import './navbar.css';
 
-export function Navbar(props){
-    /*if(!props.loggedIn){
-        return (
+export class Navbar extends React.Component{
+    render(){
+    return (
             <div className='navbar'>
                 <Link to='/'>
                     <Button
@@ -18,53 +18,38 @@ export function Navbar(props){
                 <div className='nav-group'>
                     <Link to={`/register/signup`}>
                         <Button 
-                            type="button" 
-                            label='Sign Up'
+                            label={this.props.loggedIn? '':'Sign Up'}
                             className='nav-button'
                         />
                     </Link>
                     <Link to={`/register/login`}>
                         <Button 
-                            type="button" 
-                            label='Log In'
+                            label={this.props.loggedIn ? 'Switch User':'Log In'}
                             className='nav-button'
+                        />
+                    </Link>
+                    <Link to={`/child`}>
+                        <Button 
+                            type='button'
+                            label='Child'
+                            className='nav-button' 
+                    />
+                    </Link>
+                    <Link to={`/parent`}>
+                        <Button 
+                            type='button'
+                            label='Parent'
+                            className='nav-button' 
                         />
                     </Link>
                 </div>
             </div>
         )
-    }*/
-    return(
-        <div className='navbar'>
-            <Link to='/'>
-                <Button
-                    className='home'
-                    type='button'
-                    label=''
-                />
-            </Link>
-            <div className='nav-group'>
-                <Link to={`/child`}>
-                    <Button 
-                        type='button'
-                        label='Child'
-                        className='nav-button' 
-                />
-                </Link>
-                <Link to={`/parent`}>
-                    <Button 
-                        type='button'
-                        label='Parent'
-                        className='nav-button' 
-                    />
-                </Link>
-            </div>
-        </div>
-    )
+    }
 }
 
 const mapStatetoProps = state => ({
-
+    loggedIn: state.user.data !== null,
 });
 
 export default connect(mapStatetoProps)(Navbar)
