385756e2390f7773ba089a37dd5dc6ef655b403a
Caren Keyes <cejkeyes@gmail.com>
2018-09-26 11:47:09 -0700

    finish user action tests

--- src/actions/users.test.js
+++ src/actions/users.test.js
@@ -2,7 +2,6 @@ import * as actions from './index.actions';
 import apiMiddleware from '../api-middleware';
 import asyncMiddleware from 'redux-thunk';
 import configureStore from 'redux-mock-store';
-import config from '../config';
 
 const middlewares = [
     asyncMiddleware,
@@ -11,16 +10,77 @@ const middlewares = [
 
 const mockStore = configureStore(middlewares);
 
-describe('Fetch User Info', () => {
+describe('User actions', () => {
     it('should retrieve the user info', async() => {
-        const token = 'mockToken'
+        const token = 'Bearer: mockToken'
         const response = {}
         const store = mockStore({});
-        localStorage.setItem('token', token)
+        localStorage.setItem('userToken', token)
         fetch.mockResponseOnce(JSON.stringify(response));
         await store.dispatch(actions.fetchUserBasicInfo())
         const storedActions = store.getActions()
         expect(storedActions[0]).toEqual({type: 'FETCH_USER_BASIC_INFO_REQUEST_TRIGGERED'})
         expect(storedActions[1]).toEqual({type: 'FETCH_USER_BASIC_INFO_REQUEST_SUCCESS', response})
+    })
+
+    it('should log in an existing user', async() => {
+        const store = mockStore({})
+        const response = {}
+        fetch.mockResponseOnce(JSON.stringify(response));
+        await store.dispatch(actions.fetchUserLogin('username', 'password'))
+        const storedActions = store.getActions()
+        expect(storedActions[0]).toEqual({type: 'FETCH_USER_LOGIN_REQUEST_TRIGGERED'})
+        expect(storedActions[1]).toEqual({type: 'FETCH_USER_LOGIN_REQUEST_SUCCESS', response})
+    })
+
+    it('should register a new user', async() => {
+        const store = mockStore({})
+        const user = {
+            username: 'username',
+            email: 'email@test',
+            password: 'testpassword',
+            usertype: 'parent',
+        }
+        const response = {user}
+        fetch.mockResponseOnce(JSON.stringify(response));
+        await store.dispatch(actions.registerUser(user))
+        const storedActions = store.getActions()
+        console.log(storedActions)
+        expect(storedActions[0]).toEqual({type: 'FETCH_USER_SIGNUP_REQUEST_TRIGGERED'})
+    })
+
+    it('should update user profile', async() => {
+        const store = mockStore({})
+        const response = {}
+        fetch.mockResponseOnce(JSON.stringify(response));
+        await store.dispatch(actions.updateUserProfile('userId', 'data'))
+        const storedActions = store.getActions()
+        expect(storedActions[0]).toEqual({type: 'UPDATE_USER_PROFILE_TRIGGERED'})
+        expect(storedActions[1]).toEqual({type: 'UPDATE_USER_PROFILE_SUCCESS', response})
+    })
+
+    it('should update user balance', async() => {
+        const store = mockStore({})
+        const response = {}
+        fetch.mockResponseOnce(JSON.stringify(response));
+        await store.dispatch(actions.updateUserBalance('userId', 'data'))
+        const storedActions = store.getActions()
+        expect(storedActions[0]).toEqual({type: 'UPDATE_USER_BALANCE_TRIGGERED'})
+        expect(storedActions[1]).toEqual({type: 'UPDATE_USER_BALANCE_SUCCESS', response})
+    })
+
+    it('should update child balance', async() => {
+        const store = mockStore({})
+        const response = {}
+        fetch.mockResponseOnce(JSON.stringify(response));
+        await store.dispatch(actions.updateChildBalance('userId', 'data'))
+        const storedActions = store.getActions()
+        expect(storedActions[0]).toEqual({type: 'UPDATE_CHILD_BALANCE_TRIGGERED'})
+        expect(storedActions[1]).toEqual({type: 'UPDATE_CHILD_BALANCE_SUCCESS', response})
+    })
+
+    it('Should return the logout action', () => {
+        const action = actions.logoutUserRequest();
+        expect(action.type).toEqual(actions.LOGOUT_USER_REQUEST_SUCCESS);
     })
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,13 +1,8 @@
-const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    clear: jest.fn()
-  };
-  global.localStorage = localStorageMock
-
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'jest-localstorage-mock';
 
 global.fetch = require('jest-fetch-mock');
+
 
 Enzyme.configure({adapter: new Adapter()});
