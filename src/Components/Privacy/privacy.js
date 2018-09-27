b54bdcd4b3e5019b65efcb76ac2b478e38fa9bb9
Caren Keyes <cejkeyes@gmail.com>
2018-09-27 15:05:16 -0700

    add README and screenshots

--- src/Components/Privacy/privacy.js
+++ src/Components/Privacy/privacy.js
@@ -1,28 +1,27 @@
 import React from 'react';
+import './privacy.css';
 
 export default function Privacy(){
     return (
-        <div>
+        <div className='privacy-statement'>
             <section>
                 <h3>
                     Do you like money?
                 </h3>
-                <p>
+                <p className='content-text'>
                     I'm guessing that you do. And also that you are pretty careful with it,
                     since you are here to teach your kids about budgeting and money management.
                     You probably want to be sure that your money and your identify are safe if
-                    you sign up for this app. Don't worry, I've got you covered
+                    you sign up for this app. Don't worry, we've got you covered
                 </p>
             </section>
             <section>
                 <h3>
                     Your Personal Information
                 </h3>
-                <p>
-                    I have no desire to hang on to your personal information and I will never pass
-                    it on to anyone else. I will store your username and password and your email address.
-                    The email address is just so that I can send you updates about the app or possibly 
-                    about your kids withdrawal requests. Your password is encrypted and stored securely
+                <p className='content-text'>
+                    The only personal information that Money Jars will store is your email, to inform you
+                    of updates or changes. Your password is encrypted and stored securely
                     in a database. There is no actually money involved here, just some numbers to keep track of.
                 </p>
             </section>
@@ -30,7 +29,7 @@ export default function Privacy(){
                 <h3>
                     But what about YNAB?
                 </h3>
-                <p>
+                <p className='content-text'>
                     So here's the really cool part. Money Jars won't ever see your YNAB logon information.
                     You will tell YNAB that it's cool for Money Jars to see how much money you have 
                     in your budget (specifically we're looking for the kid category that you choose) and 
@@ -42,7 +41,7 @@ export default function Privacy(){
             </section>
             <section>
                 <h3>Sound Good?</h3>
-                <p>
+                <p className='content-text'>
                     Sign up for an account or start budgeting with your kids now!
                 </p>
             </section>
