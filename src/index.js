e547e3b36ccef116ca7e7ead0e9fa6a19d2e0569
Caren Keyes <cejkeyes@gmail.com>
2018-10-31 16:11:29 -0700

    initial attempt at redux-persist integration

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import {Provider} from 'react-redux';
 import './index.css';
 import App from './Components/App/App';
 import store from './store';
+import {persistor} from './store';
 import registerServiceWorker from './registerServiceWorker';
+import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
     <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <App />
-        </ConnectedRouter>
+        <PersistGate loading={null} persistor={persistor}>
+            <ConnectedRouter history={history}>
+                <App />
+            </ConnectedRouter>
+        </PersistGate>
     </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware} from 'redux';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import apiMiddleware from './api-middleware';
 import createHistory from 'history/createBrowserHistory';
@@ -8,9 +10,16 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 
 
 export const history = createHistory();
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+//const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const initialState = {};
 const middleware = [
+  persistReducer(persistConfig),
   thunk,
   apiMiddleware,
   routerMiddleware(history),
@@ -27,4 +36,6 @@ const store = createStore(
   enhancers,
 );
 
-export default store;
+export const persistor = persistStore(store)
+
+export default {store};
